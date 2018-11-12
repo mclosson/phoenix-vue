e1cf137aeb19d2d15d3e0e73df473ede8109eeae
Matthew Closson <matthew.closson@ctg.com>
2018-11-12 11:42:08 -0500

    Add basic Vue use to index page

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,12 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import Vue from "vue/dist/vue.common.js"
+
+new Vue({
+  el: "#hello-vue",
+  data: {
+    message: "Hello Vue!"
+  }
+});
